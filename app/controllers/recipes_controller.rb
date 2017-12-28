df2dd2bc8a090d153fb6fd7f5cf957a553442da7
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-28 23:07:46 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,8 +22,8 @@ class RecipesController < ApplicationController
     params.require(:recipe).permit(
     :title,
     ingredients_attributes: [
-      :ingredient_name,
-      :ingredient_quantity
+      :name,
+      :quantity
     ]
     )
   end
